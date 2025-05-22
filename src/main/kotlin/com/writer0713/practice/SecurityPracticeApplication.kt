e0f9df17629d2920ac886a4ca394a8060a2e0375
KIM JUNGHWAN <tyler.kim@navercorp.com>
2025-05-22 20:49:18 +0900

    Initial commit

---
+++ src/main/kotlin/com/writer0713/practice/SecurityPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.writer0713.practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecurityPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SecurityPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/writer0713/practice/SecurityPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.writer0713.practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecurityPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
